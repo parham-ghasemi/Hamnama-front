82eae97918b6b6ce2b86deda90b74bffd613636d
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2026-07-24 03:59:50 +0330

    Added plan users

--- src/pages/userDashboard/planUsers/PlanUsers.tsx
+++ src/pages/userDashboard/planUsers/PlanUsers.tsx
@@ -1,10 +1,62 @@
+import { BsPlusLg } from 'react-icons/bs';
 import './PlanUsers.scss';
 
 const PlanUsers = () => {
+  const users = [
+    {
+      pfp: "",
+      name: "Mamad",
+      daysSinceJoined: 42,
+    },
+    {
+      pfp: "",
+      name: "Mehdi",
+      daysSinceJoined: 42,
+    },
+    {
+      pfp: "/rodeocover.png",
+      name: "Kassraa",
+      daysSinceJoined: 42,
+    },
+    {
+      pfp: "",
+      name: "Mamad",
+      daysSinceJoined: 42,
+    },
+  ]
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center text-3xl font-black' dir='ltr'>
-      THIS PAGE IS CURRENTLY BEING DEVELOPED
-      <span className='text-xs font-light'>wait a lil damn, i'm working on it :(</span>
+    <div className="plan-users">
+      <div className="plan-users__cards">
+        {
+          users.map((user, ind) => (
+            <div className="plan-users__cards__card" key={ind}>
+              <div className="plan-users__cards__card__user-info">
+                {
+                  user.pfp ? (
+                    <img src={user.pfp} alt={`${user.name}'s profile picture`} />
+                  ) : (
+                    <p>{user.name[0]}</p>
+                  )
+                }
+                <span>{user.name}</span>
+              </div>
+
+              <div className="plan-users__cards__card__since">
+                {`مدت مشترک شدن: ${user.daysSinceJoined} روز`}
+              </div>
+
+              <button className='plan-users__cards__card__kick'>اخراج کاربر</button>
+            </div>
+          ))
+        }
+
+        <div className="plan-users__cards__card plan-users__cards__card--add">
+          <span className="plan-users__cards__card__add-icon">
+            <BsPlusLg strokeWidth={1} />
+          </span>
+          <p className="plan-users__cards__card__add-text">افزودن کاربر</p>
+        </div>
+      </div>
     </div>
   )
 }
